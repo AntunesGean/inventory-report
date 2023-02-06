27f0cbb40b0aa0728774732bace3887955791014
AntunesGean <gean.antunes@gmail.com>
2023-02-06 10:32:36 -0300

    req 02 - ok

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,18 @@
+from statistics import mode
+
+
 class SimpleReport:
-    pass
+
+    @classmethod
+    def generate(self, list):
+
+        manufac_date = min([prod["data_de_fabricacao"] for prod in list])
+        expirat_date = min([prod["data_de_validade"] for prod in list])
+        more_products = mode(
+            [prod["nome_da_empresa"] for prod in list])
+
+        return(
+            f"Data de fabricação mais antiga: {manufac_date}\n"
+            f"Data de validade mais próxima: {expirat_date}\n"
+            f"Empresa com mais produtos: {more_products}"
+        )
