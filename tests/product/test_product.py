2de45a2b0b22c4baf0b343e116cd8a04af19d2cd
AntunesGean <gean.antunes@gmail.com>
2023-02-06 10:20:13 -0300

    iniciando o projeto & req 01 - ok

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,22 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    pass
+    product = Product(
+        1,
+        "CD Molejo 25 anos - O Baile É Sem Parar",
+        "Columbia Records",
+        "10/04/2014",
+        "31/12/2099",
+        "Molejo",
+        "na Caçamba"
+        )
+
+    assert product.id == 1
+    assert product.nome_do_produto == "CD Molejo 25 anos - O Baile É Sem Parar"
+    assert product.nome_da_empresa == "Columbia Records"
+    assert product.data_de_fabricacao == "10/04/2014"
+    assert product.data_de_validade == "31/12/2099"
+    assert product.numero_de_serie == "Molejo"
+    assert product.instrucoes_de_armazenamento == "na Caçamba"
