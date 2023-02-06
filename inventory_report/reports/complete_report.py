aed9c77c7178dc7d166806d8fddfddcac7f30af2
AntunesGean <gean.antunes@gmail.com>
2023-02-06 10:43:35 -0300

    req 03 - ok

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,20 @@
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
 class CompleteReport:
-    pass
+
+    def generate(list):
+
+        simple_report = SimpleReport.generate(list)
+        company_products = Counter(prod["nome_da_empresa"] for prod in list)
+        stock = ""
+
+        for company_n, company_quantity in company_products.items():
+            stock += f"- {company_n}: {company_quantity}\n"
+
+        return (
+            f"{simple_report}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{stock}"
+        )
